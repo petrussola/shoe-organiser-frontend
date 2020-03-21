e37b18d855cf54fa8e360c864ac534707fc14924
Pere <pere.solaclaver@gmail.com>
2020-03-21 22:14:15 +0000

    Build script

---
+++ build/precache-manifest.5e93620ad3121c43395747f4ecd0d5b4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5be1258a00b2b83adc56d22dfdc583b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2ea0d97f1023f0b262cb",
+    "url": "/static/css/main.523f09c5.chunk.css"
+  },
+  {
+    "revision": "d21b713d82d3196b3cf4",
+    "url": "/static/js/2.ea5181e1.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.ea5181e1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2ea0d97f1023f0b262cb",
+    "url": "/static/js/main.f695a13c.chunk.js"
+  },
+  {
+    "revision": "7c4530d5950eb3cee65c",
+    "url": "/static/js/runtime-main.6fc00b47.js"
+  }
+]);
