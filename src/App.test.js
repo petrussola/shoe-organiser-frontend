d138c4efa765a03534311ff94947366d19094b04
Pere <pere.solaclaver@gmail.com>
2020-05-09 16:43:34 +0100

    Add router to test

--- src/App.test.js
+++ src/App.test.js
@@ -1,10 +1,15 @@
 /* eslint-disable no-undef */
 import React from 'react';
 import { render } from '@testing-library/react';
+import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
 
 test('renders learn react link', () => {
-	const { getByText } = render(<App />);
+	const { getByText } = render(
+		<Router>
+			<App />
+		</Router>
+	);
 	const linkElement = getByText(/Register/i);
 	expect(linkElement).toBeInTheDocument();
 });
